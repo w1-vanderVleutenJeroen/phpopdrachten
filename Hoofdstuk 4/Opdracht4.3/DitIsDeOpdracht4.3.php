29bfab988a30a19aedc71bad019b559d9aa9babf
Jeroen van der Vleuten <jeroen.vandervleuten@edu-kw1c.nl>
2020-03-15 16:37:51 +0100

    Hoofdstuk 4.3

---
+++ Hoofdstuk 4/Opdracht4.3/DitIsDeOpdracht4.3.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: Jeroen van der Vleuten
+ * Date: 06-03-2020
+ * Time: 11:51
+ * File: opdracht4.3.php
+ */
+include "../../includes/header.php";
+include "Opdracht4.3.php";
+
+echo $task2;
+?>
+<br><br>
+<?php
+echo $task3;
+?>
+<br><br>
+<?php
+echo $task4;
+?>
+    <br><br>
+<?php
+echo $task5;
+
+?>
+    <br><br>
+<?php
+echo $task6;
+?>
+<br><br>
+<?php
+echo $task7;
+?>
+
+
+
